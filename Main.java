c1031fa511c9254603a7ed1a175ec2cbf903bb71
Anastasia <slavjanka54@mail.ru>
2023-10-22 22:37:05 +0700

    исправление 3

---
+++ Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    /**
+     * @param args
+     * @param phoneBook TODO
+     */
+    public static void main(String[] args, PhoneBook phoneBook) {
+        String name1;
+        String name4;
+        String name2;
+        String name3;
+        String name5;
+
+        int phone1;
+        int phone2;
+        int phone3;
+
+        name1 = "Иванов";
+        name2 = "Петров";
+        name3 = "Сидоров";
+        name4 = "Васечкин";
+        name5 = "Попов";
+        phone1 = 123456;
+        phone2 = 654321;
+        phone3 = 4654321;
+
+        var myPhoneBook = new PhoneBook();
+        Object put;
+        myPhoneBook.add(name1, phone1, put);
+        myPhoneBook.add(name1, phone2, put);
+        myPhoneBook.add(name1, phone2, phoneBook    myPhoneBook.add(name1, phone3, put);
+        myPhoneBook.add(name2, phone2, put);
+        myPhoneBook.add(name3, phone2, put);
+        myPhoneBook.add(name3, phone1, put);
+        myPhoneBook.add(name4, phone1, put);
+        myPhoneBook.add(name5, phone1, put);
+
+        System.out.println(myPhoneBook.find(name1));
+        System.out.println("**************************");
+        PhoneBook.getPhoneBook();
+        System.out.println("*************************");
+        System.out.println(myPhoneBook.find("Веном"));
+    }
+}
